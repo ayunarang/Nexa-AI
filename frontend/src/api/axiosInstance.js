ca3b7249917faa7d6ca2a665096f7b3e0b81596c
ayunarang <ayushinarang21@gmail.com>
2025-07-20 08:31:19 +0530

    Changed backend endpoint

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "sonner";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api", 
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 const initSession = async () => {
